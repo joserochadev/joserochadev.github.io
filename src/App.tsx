c7ce8c195b875b04b209a82b2a6351fecb0e007e
joserochadev <joserochadev@gmail.com>
2026-02-20 11:38:31 -0300

    feat: add VLibras Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { LanguageProvider } from "./contexts/language-context";
-import { ThemeProvider } from "./contexts/theme-context";
-import { AboutSection } from "./pages/about-section";
-import { ContactSection } from "./pages/contact-section";
-import { Footer } from "./pages/footer";
-import { Header } from "./pages/header";
-import { HeroSection } from "./pages/hero-section";
-import { ProjectsSection } from "./pages/projects-section";
+import { LanguageProvider } from './contexts/language-context'
+import { ThemeProvider } from './contexts/theme-context'
+import { AboutSection } from './pages/about-section'
+import { ContactSection } from './pages/contact-section'
+import { Footer } from './pages/footer'
+import { Header } from './pages/header'
+import { HeroSection } from './pages/hero-section'
+import { ProjectsSection } from './pages/projects-section'
 
 export function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <LanguageProvider>
-        <Header />
-        <HeroSection />
-        <AboutSection />
-        <ProjectsSection />
-        <ContactSection />
-        <Footer />
-      </LanguageProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<LanguageProvider>
+				<Header />
+				<HeroSection />
+				<AboutSection />
+				<ProjectsSection />
+				<ContactSection />
+				<Footer />
+			</LanguageProvider>
+		</ThemeProvider>
+	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles/globals.css";
-import { App } from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/globals.css'
+import { App } from './App.tsx'
+import VLibras from 'vlibras-nextjs'
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<App />
+		<VLibras forceOnload />
+	</StrictMode>
+)
